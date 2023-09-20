ce330d41641c600a9f0b4a42b7874608c3b03e56
cprthompson <cprthompson@gmail.com>
2023-09-20 12:35:08 -0500

    modifications to add typescript (#1)
    * modifications to add typescript
    
    ---------
    
    Co-authored-by: Cooper Thompson <cthompson@Kathryns-MacBook-Air.local>

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,17 @@ import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date';
+import { GetStaticProps } from 'next';
 
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData,
+}: {
+    allPostsData: {
+    date: string;
+    title: string;
+    id: string; 
+  }[];
+}) {
   return (
     <Layout home>
       <Head>
@@ -26,7 +35,7 @@ export default function Home({ allPostsData }) {
               <Link href={`/posts/${id}`}>{title}</Link>
               <br />
               <small className={utilStyles.lightText}>
-                <Date dateString={date} />
+                <Date theDate={date} />
               </small>
             </li>
           ))}
@@ -36,11 +45,11 @@ export default function Home({ allPostsData }) {
   );
 }
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData();
   return {
     props: {
       allPostsData,
     }
-  }
-}
+  };
+};
